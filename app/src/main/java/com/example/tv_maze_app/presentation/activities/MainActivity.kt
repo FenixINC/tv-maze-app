8a2285918a12289bd5bd8f05b0b2dd7ba471af28
Taras Koloshmatin <taras@pier2marketing.com>
2019-09-27 00:05:13 +0300

    clean up

--- app/src/main/java/com/example/tv_maze_app/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/tv_maze_app/presentation/activities/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.tv_maze_app.presentation.activities
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -58,21 +56,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                mBinding.drawerLayout.openDrawer(GravityCompat.START)
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
